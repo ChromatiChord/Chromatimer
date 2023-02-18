000de5c35c30a9ccb7ee66fefa5884caf2962b67
Callum Ward <callumward56@gmail.com>
2023-02-18 16:34:40 +1100

    Added minor comments for code clarity

--- decoratortimer.py
+++ decoratortimer.py
@@ -3,20 +3,22 @@ from time import perf_counter
 def decorator_timer(interval='s'):
     def decorator(func):
         def wrapper(*args, **kwargs):
+            # runs the function and times the output
             start_time = perf_counter()
             func(*args, **kwargs)
-            time = perf_counter() - start_time
+            total_time = perf_counter() - start_time
 
+            # determines what time inteval should be displayed
             if interval == 'ms':
-                time *= 1_000
+                total_time *= 1_000
             elif interval == 'µs':
-                time *= 1_000_000
+                total_time *= 1_000_000
             elif interval == 'ns':
-                time *= 1_000_000_000
+                total_time *= 1_000_000_000
             else:
                 if interval != "s":
                     raise Exception("Decorator timer input error. Accepted values: [\"s\",\"ms\",\"µs\",\"ns\"]")
             
-            print(f"Function \'{func.__name__}\' took {round(time, 5)}{interval}")
+            print(f"Function \'{func.__name__}\' took {round(total_time, 4)}{interval}.")
         return wrapper
     return decorator
