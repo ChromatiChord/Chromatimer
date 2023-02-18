50520c1c24c3f948f4b514a99b88b4955a9e1bdf
Callum Ward <callumward56@gmail.com>
2023-02-18 16:21:31 +1100

    Finalised interval adjustment functionality

--- decoratortimer.py
+++ decoratortimer.py
@@ -1,11 +1,22 @@
 from time import perf_counter
 
-def decorator_timer(output=False):
+def decorator_timer(interval='s'):
     def decorator(func):
         def wrapper(*args, **kwargs):
             start_time = perf_counter()
             func(*args, **kwargs)
-            if output:
-                print(f"Function took {perf_counter() - start_time}s")
+            time = perf_counter() - start_time
+
+            if interval == 'ms':
+                time *= 1_000
+            elif interval == 'µs':
+                time *= 1_000_000
+            elif interval == 'ns':
+                time *= 1_000_000_000
+            else:
+                if interval != "s":
+                    raise Exception("Decorator timer input error. Accepted values: [\"s\",\"ms\",\"µs\",\"ns\"]")
+            
+            print(f"Function \'{func.__name__}\' took {round(time, 5)}{interval}")
         return wrapper
     return decorator
